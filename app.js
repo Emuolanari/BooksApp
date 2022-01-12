e15193b1c35f81158e9cbee10a1734b81d01fd2c
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-01-12 23:19:18 +0000

    xss & mongodb injection security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const bookRouter = require('./routes/bookRouter');
@@ -8,6 +9,8 @@ const bookRouter = require('./routes/bookRouter');
 const app = express();
 
 app.use(express.json());
+app.use(xss());
+app.use(mongoSanitize());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
