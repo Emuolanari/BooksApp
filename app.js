6ffc4c0b40fb2a29024e1cf54749b252dfb594ff
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-01-14 22:57:27 +0000

    CORS

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const bookRouter = require('./routes/bookRouter');
 
 const app = express();
 
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use(express.json());
 app.use(xss());
 app.use(mongoSanitize());
